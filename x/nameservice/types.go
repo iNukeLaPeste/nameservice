a1d7a58b97cb1849c3ee453a5e9614aba041879b
iNukeLaPeste <mathis.selvi@epitech.eu>
2019-04-10 18:57:02 +0200

    go.mod fixxx

--- x/nameservice/types.go
+++ x/nameservice/types.go
@@ -1,11 +1,11 @@
 package nameservice
 
 import (
-	"fmt"
-	"strings"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+// Initial Starting Price for a name that was never previously owned
+var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
 
 // Whois is a struct that contains all the metadata of a name
 type Whois struct {
@@ -14,12 +14,10 @@ type Whois struct {
 	Price sdk.Coins      `json:"price"`
 }
 
-// Initial Starting Price for a name that was never previously owned
-var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
-
 // Returns a new Whois with the minprice as the price
 func NewWhois() Whois {
 	return Whois{
 		Price: MinNamePrice,
 	}
 }
+
